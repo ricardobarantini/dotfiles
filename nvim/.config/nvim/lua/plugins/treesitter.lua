5164b125693476a444ea41f99bed96936f8744bc
Ricardo Barantini <ricardobarantini@gmail.com>
2025-05-19 16:17:55 -0300

    nvim update

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -4,9 +4,9 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = { "lua", "php", "html", "javascript", "css", "bash" },
+      ensure_installed = { "lua", "php", "html", "javascript", "css", "scss", "json", "bash" },
       highlight = { enable = true },
-      indent = { enable = true },
+      indent = { enable = false },
     })
   end
 }
