ee0688c0e3b33cdfdd094331d1e7074c0ba0e137
Ricardo Barantini <ricardobarantini@gmail.com>
2025-05-19 14:04:00 -0300

    nvim configuration

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      ensure_installed = { "lua", "php", "html", "javascript", "css", "bash" },
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end
+}
