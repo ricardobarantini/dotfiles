1625fa48fa636594a8f046d58adbb1252e8232a6
Ricardo Barantini <ricardobarantini@gmail.com>
2025-05-20 13:35:29 -0300

    nvim telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -5,10 +5,15 @@ return {
     require('telescope').setup {
       defaults = {
         file_ignore_patterns = { "node_modules", "vendor" }
-      }
+      },
+      pickers = {
+        find_files = {
+          hidden = true
+        },
+      },
     }
     local builtin = require("telescope.builtin")
-    vim.keymap.set('n', '<C-f>', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
   end
 }
