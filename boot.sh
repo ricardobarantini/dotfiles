408a265cd47e3ec3bb97944bb6084e6dceaf55b2
Ricardo Barantini <ricardobarantini@gmail.com>
2025-07-29 12:07:16 -0300

    remove installation scripts

--- boot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -fr ~/.local/share/dotfiles
-git clone git@github.com:ricardobarantini/dotfiles.git ~/.local/share/dotfiles > /dev/null
-
-source ~/.local/share/dotfiles/install.sh
