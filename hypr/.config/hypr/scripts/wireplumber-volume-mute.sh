de7084b2133eddd86ba28485673ad41f077200dc
Ricardo Barantini <ricardobarantini@gmail.com>
2025-08-02 14:12:28 -0300

    volume notifications

---
+++ hypr/.config/hypr/scripts/wireplumber-volume-mute.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle &&
+  bash ~/.config/hypr/scripts/mako-volume.sh
---
+++ hypr/.config/hypr/scripts/wireplumber-volume-up.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+ &&
+  bash ~/.config/hypr/scripts/mako-volume.sh
