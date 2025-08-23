8e9d8c5c0048c7d1ed0b1ff3057656da94cc4630
Ricardo Barantini <ricardobarantini@gmail.com>
2025-08-23 12:54:20 -0300

    update swaybg script

--- scripts/.local/bin/swaybg.sh
+++ scripts/.local/bin/swaybg.sh
@@ -8,4 +8,5 @@ fi
 
 WALLPAPER=$(find $WALLPAPERS_DIR -type f ! -path "*/.git/*" ! -path "*.md" | shuf -n 1)
 
-uwsm app -- swaybg --image $WALLPAPER -m fill
+pkill -x swaybg
+setsid uwsm app -- swaybg --image $WALLPAPER -m fill >/dev/null 2>&1 &
