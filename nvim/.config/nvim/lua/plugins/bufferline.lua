9dd1906466b3158eed1a99f0ecfb6b651e013649
Ricardo Barantini <ricardobarantini@gmail.com>
2025-09-18 22:22:49 -0300

    remove nvim config files

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "akinsho/bufferline.nvim",
-  init = function()
-    local bufferline = require("catppuccin.groups.integrations.bufferline")
-    function bufferline.get()
-      return bufferline.get_theme()
-    end
-  end,
-}
