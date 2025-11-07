06ee75f3003ab5e182e1187ae46654a7582461cd
Ricardo Barantini <ricardobarantini@gmail.com>
2025-11-07 18:46:03 -0300

    waybar: few improvements, layout update

--- waybar/.config/waybar/scripts/toggle-scheme.sh
+++ waybar/.config/waybar/scripts/toggle-scheme.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 check_mode() {
   local current_scheme=$(gsettings get org.gnome.desktop.interface color-scheme)
@@ -15,13 +15,17 @@ toggle_mode() {
 
   if [[ $current_scheme == "'prefer-dark'" ]]; then
     gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
+    MESSAGE="Interface scheme set to light"
   else
     gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+    MESSAGE="Interface scheme set to dark"
   fi
 }
 
 if [[ "$1" == "toggle" ]]; then
   toggle_mode
+
+  notify-send "Color Scheme" "$MESSAGE"
 fi
 
 check_mode
